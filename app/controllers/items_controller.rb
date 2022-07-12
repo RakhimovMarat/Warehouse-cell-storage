e2165674719dfe3bbc613585bd805aaf72b28697
marat <mf.rakhimov1982@gmail.com>
2022-07-12 20:31:03 +0300

    Update item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+  def index
+    @item = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:number, :description)
+  end
 end
