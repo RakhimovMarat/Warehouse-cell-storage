2540512839e4037846464658fdbcb122d4234849
marat <mf.rakhimov1982@gmail.com>
2022-07-12 20:19:09 +0300

    Updating routes

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,10 @@
 ActiveRecord::Schema[7.0].define(version: 2022_07_11_155355) do
   create_table "addresses", force: :cascade do |t|
     t.string "name"
+    t.integer "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_addresses_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -24,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_155355) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "items"
 end
