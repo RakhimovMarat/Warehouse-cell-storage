090f61936dabea9f095d5159f33ad36276447abb
marat <mf.rakhimov1982@gmail.com>
2022-07-12 20:30:45 +0300

    Update address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,26 @@
 class AddressesController < ApplicationController
+  def index
+    @address = Address.all
+  end
+
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    @item = Address.new(address_params)
+    if @address.save
+      redirect_to @address
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name)
+  end
 end
